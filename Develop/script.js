c273e42339e705d54b43bd67005eb5b3acd4ed15
Ellin Tj <tjellin@gmail.com>
2020-01-12 19:53:34 -0800

    cleaning up codes in script.js

--- Develop/script.js
+++ Develop/script.js
@@ -10,10 +10,10 @@ function generatePassword() {
         let pswdLength = prompt("Please enter the length of the password. Size must be between 8-128");
         while (correct) {
           if (pswdLength < 8) {
-            pswdLength = prompt("Sorry, the number " + pswdLength + " is smaller than 8.\n You need to enter a number between 8 and 128.\n Please enter the length of the password.");
+            pswdLength = prompt("Sorry, the number " + pswdLength + " is smaller than 8.\n You need to enter a number between 8 and 128.\nPlease enter the length of the password.");
           }
           else if (pswdLength > 128) {
-            pswdLength = prompt("Sorry, the number " + pswdLength + " is bigger than 128.\n You need to enter a number between 8 and 128.\n Please enter the length of the password.");
+            pswdLength = prompt("Sorry, the number " + pswdLength + " is bigger than 128.\n You need to enter a number between 8 and 128.\nPlease enter the length of the password.");
           }
           else
             correct = false;
@@ -21,56 +21,34 @@ function generatePassword() {
 
 
         let shuffleThis = lowerCase;
-        for (let i = 0; i < shuffleThis.length; i++) {
-          console.log("shuffleThis[" + i + "] = " + shuffleThis[i]);
-        }
 
         isSpecialChar = confirm("Do you want to have special characters in the password?");
         if (isSpecialChar) {    
-          console.log("**************inside isSpecialChar");    
           const specialChar = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
-
           shuffleThis = shuffleThis.concat(specialChar);
-          for (let i = 0; i < shuffleThis.length; i++) {
-            console.log("shuffleThis[" + i + "] = " + shuffleThis[i]);
-          }
         }
 
         isNumericChar = confirm("Do you want to have numeric characters in the password?");
         if (isNumericChar) {
-          console.log("**************inside isNumericChar");
           for (let i = 0; i<=9; i++) {
             shuffleThis.push(i);
-          }
-          for (let i = 0; i < shuffleThis.length; i++) {
-            console.log("shuffleThis[" + i + "] = " + shuffleThis[i]);
           }
         }
 
         isUpperLowerCaseChar = confirm("Do you want to have a combination of lower and upper case letters in the password? Note: The default would be all lower case alphabet only.");
         if (isUpperLowerCaseChar) { 
-          console.log("**********inside isUpperLowerCaseChar");
-          const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-          
-          shuffleThis = shuffleThis.concat(upperCase);
-          for (let i = 0; i < shuffleThis.length; i++) {
-            console.log("shuffleThis[" + i + "] = " + shuffleThis[i]);
-          }
+          const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];shuffleThis = shuffleThis.concat(upperCase);
         }
 
         let randomPassword = "";
         // Generating the random password
         for (let i = 0; i < pswdLength; i++) {
           var num = Math.floor(Math.random() * shuffleThis.length);
-          // Display in console
           randomPassword = randomPassword + shuffleThis[num];
         }
-  console.log(randomPassword);
 
   return randomPassword;
 }
-//**********Ellin's code starts here *******/
-
 //**********Ellin's code ends here *******/
 
 // Write password to the #password input
